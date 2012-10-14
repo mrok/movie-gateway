3fccd9389aba15eceb8e757742b9e2127ea4dd05
serek <serek@serek-lapek.(none)>
2012-10-14 11:28:16 +0200

    Improve error reporting, fix for travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,8 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
 
 $app = require __DIR__ . '/../app/app.php';
 $app['debug'] = true;
-
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 1);
 
 return $app;
--- web/index.php
+++ web/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 0);
 
 $app = require __DIR__.'/../app/app.php';
 $app->run();
